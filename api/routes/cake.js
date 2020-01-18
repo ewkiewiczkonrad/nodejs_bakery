3726727dcd3fd5af9bd846192bf419b6d638f429
Konrad Ewkiewicz <konrad.ewkiewicz@gmail.com>
2020-01-18 11:01:15 +0100

    Parsing the body

--- api/routes/cake.js
+++ api/routes/cake.js
@@ -8,8 +8,13 @@ router.get('/', (req,res,next) => {
 });
 
 router.post('/', (req,res,next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
     res.status(201).json({
-        message:"post request to /cake"
+        message:"post request to /cake",
+        createdProduct: product
     })
 });
 
--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const cakeRoutes = require('./api/routes/cake');
 const toBakeRoutes = require('./api/routes/toBake');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.getHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type, Accept, Authorization");
 
+if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+}
+});
 app.use('/cake', cakeRoutes);
 app.use('/toBake', toBakeRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
-    error.status=404;
+    error.status = 404;
     next(error);
-});
+})
 
-app.use((error, req, res, next) =>{
-    res.status(err.status ||500);
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
     res.json({
-        error:{
+        error: {
             message: error.message
         }
     });
